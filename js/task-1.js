8e67e526f7b50683ed0305fe9f43240e9719abd2
Uliana Halushchak <ulyana.kutsir@gmail.com>
2025-05-29 16:15:32 +0300

    add files

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+const ulElems = document.querySelector('#categories');
+const liElems = document.querySelectorAll('.item')
+console.log(`Number of categories: ${liElems.length}`);
+
+
+for (let i = 0; i < liElems.length; i++) {
+    const item = liElems[i];
+    const title = item.querySelector("h2").textContent;
+    const subitems = item.querySelectorAll("ul li").length;
+console.log(`Category: ${title}`);
+console.log(`Elements: ${subitems}`);
+}
